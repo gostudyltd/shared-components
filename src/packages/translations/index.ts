3ab4b849607431d783794cc5c2279bbe0e41855e
Anton Tsvirko <a.tsvirko@mypath.group>
2023-08-03 15:18:41 +0300

    Add missed export & update version to 0.0.11

--- src/packages/translations/index.ts
+++ src/packages/translations/index.ts
@@ -4,4 +4,5 @@ export * from './IntroSection';
 export * from './List5Section';
 export * from './List6Section';
 export * from './ImageWithTitleTwoColumnsSection';
+export * from './FormSection';
 export * from './utils';
