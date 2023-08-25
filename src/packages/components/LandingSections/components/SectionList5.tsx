19d457a529847c44537b20944681edc668115620
Miroslav <m.masevich@mypath.group>
2023-08-25 13:33:07 +0300

    change tarif card component, add disabled props. update version

--- src/packages/components/LandingSections/components/SectionList5.tsx
+++ src/packages/components/LandingSections/components/SectionList5.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Box, Button, ButtonProps, Stack, Typography } from '@mui/material';
-import { AccentColor } from './types';
-import { accentColorBase } from './utils';
-import { CheckInCircle } from '../../Icons/CheckInCircle';
-import { withCustomTheme } from '../../hoc/withCustomTheme';
+import React from "react";
+import { Box, Button, ButtonProps, Stack, Typography } from "@mui/material";
+import { AccentColor } from "./types";
+import { accentColorBase } from "./utils";
+import { CheckInCircle } from "../../Icons/CheckInCircle";
+import { withCustomTheme } from "../../hoc/withCustomTheme";
 // import CheckRoundedIcon from '@mui/icons-material/CheckRounded'
 
 type DataItem = {
@@ -14,6 +14,7 @@ type DataItem = {
   button?: {
     text: string;
     onClick?: () => void;
+    disabled?: boolean;
   };
 
   /** From MUI */
@@ -34,73 +35,73 @@ const CardItem: React.FC<CardItemProps> = (props) => {
   const { title } = data;
   const accent = accentColorBase.primary;
   return (
-    <Stack sx={{ height: 'inherit', flex: '1 1 0', pt: '42px' }}>
+    <Stack sx={{ height: "inherit", flex: "1 1 0", pt: "42px" }}>
       <Stack
         sx={{
-          p: '1.25rem',
-          backgroundColor: 'background.paper',
-          borderRadius: '1.25rem',
-          position: 'relative',
-          height: '100%',
+          p: "1.25rem",
+          backgroundColor: "background.paper",
+          borderRadius: "1.25rem",
+          position: "relative",
+          height: "100%",
         }}
       >
         <Typography
-          variant='h3'
-          lineHeight={'1.44'}
+          variant="h3"
+          lineHeight={"1.44"}
           sx={{
-            mb: '.5rem',
-            fontWeight: '600',
+            mb: ".5rem",
+            fontWeight: "600",
             color: accent.main,
-            fontSize: { xs: '1.25rem', sm: '1.75rem' },
+            fontSize: { xs: "1.25rem", sm: "1.75rem" },
           }}
         >
           {title}
         </Typography>
         <Typography
           sx={{
-            mb: { xs: '1.5rem', sm: '2.5rem' },
-            fontSize: { xs: '0.875rem', sm: '1rem' },
-            color: 'rgba(0,0,0,0.54)',
+            mb: { xs: "1.5rem", sm: "2.5rem" },
+            fontSize: { xs: "0.875rem", sm: "1rem" },
+            color: "rgba(0,0,0,0.54)",
           }}
         >
           {data.description}
         </Typography>
 
-        <Stack mb={'auto'}>
+        <Stack mb={"auto"}>
           {data.list.map((text) => (
             <Stack
-              direction={'row'}
-              alignItems={'flexStart'}
+              direction={"row"}
+              alignItems={"flexStart"}
               key={text}
               sx={{
-                '& + *': {
-                  marginTop: '0.625rem',
+                "& + *": {
+                  marginTop: "0.625rem",
                 },
               }}
             >
               <Box
-                component='span'
-                display={'flex'}
-                alignItems={'center'}
-                justifyContent={'center'}
-                flexShrink={'0'}
+                component="span"
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                flexShrink={"0"}
                 sx={{
-                  marginRight: '.75rem',
-                  height: '1.5rem',
-                  width: '1.5rem',
+                  marginRight: ".75rem",
+                  height: "1.5rem",
+                  width: "1.5rem",
                   backgroundColor: accent.light,
-                  lineHeight: '1.43',
-                  borderRadius: '50%',
+                  lineHeight: "1.43",
+                  borderRadius: "50%",
                 }}
               >
                 <CheckInCircle />
               </Box>
               <Typography
-                variant='h5'
+                variant="h5"
                 sx={{
                   fontSize: {
-                    xs: '1.125rem',
-                    sm: '1rem',
+                    xs: "1.125rem",
+                    sm: "1rem",
                   },
                 }}
               >
@@ -111,17 +112,18 @@ const CardItem: React.FC<CardItemProps> = (props) => {
         </Stack>
         {data.button && (
           <Button
-            variant='contained'
-            size='large'
+            variant="contained"
+            size="large"
             onClick={data.button.onClick}
+            disabled={data.button.disabled}
             sx={{
-              fontSize: { xs: '1rem', sm: '1.125rem' },
-              height: { xs: '2.625rem', sm: '3.5rem' },
+              fontSize: { xs: "1rem", sm: "1.125rem" },
+              height: { xs: "2.625rem", sm: "3.5rem" },
               backgroundColor: accent.light,
-              justifySelf: 'flex-end',
+              justifySelf: "flex-end",
               mt: {
-                xs: '1.5rem',
-                sm: '2.5rem',
+                xs: "1.5rem",
+                sm: "2.5rem",
               },
             }}
           >
@@ -130,23 +132,23 @@ const CardItem: React.FC<CardItemProps> = (props) => {
         )}
 
         <Box
-          position={'absolute'}
+          position={"absolute"}
           sx={{
-            py: '.5rem',
-            px: { sm: '1.5rem', xs: '1.25rem' },
-            top: '-42px',
-            left: '0',
-            height: '107px',
-            borderRadius: '1.25rem',
-            backgroundColor: '#FBC02D',
-            zIndex: '-1',
+            py: ".5rem",
+            px: { sm: "1.5rem", xs: "1.25rem" },
+            top: "-42px",
+            left: "0",
+            height: "107px",
+            borderRadius: "1.25rem",
+            backgroundColor: "#FBC02D",
+            zIndex: "-1",
           }}
         >
           <Typography
-            variant='h5'
+            variant="h5"
             sx={{
-              fontSize: { xs: '1rem', sm: '1.125rem' },
-              color: 'background.paper',
+              fontSize: { xs: "1rem", sm: "1.125rem" },
+              color: "background.paper",
             }}
           >
             {data.badge}
@@ -162,8 +164,8 @@ export const SectionList5: React.FC<SectionList5Props> = withCustomTheme(
     const { data } = props;
     return (
       <Stack
-        gap={{ xs: '1.25rem', sm: '1.5rem' }}
-        direction={{ sm: 'row', xs: 'column' }}
+        gap={{ xs: "1.25rem", sm: "1.5rem" }}
+        direction={{ sm: "row", xs: "column" }}
       >
         {data.map((i) => (
           <CardItem key={i.title} data={i} />
